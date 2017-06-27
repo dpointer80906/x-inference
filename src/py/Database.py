299a1e1268854ea4d5085b351e8399daf0dcdf99
David Pointer <dpointer@directstream.com>
2017-06-27 13:46:48 -0600

    Initial release of Database class.

--- src/py/Database.py
+++ src/py/Database.py
@@ -1,61 +1,70 @@
-'''
+'''Interface to FoW tank team authoritative fact database.
 
+TODO: extend this out beyond just tank teams.
 '''
+import logging
 import sqlite3
 from sqlite3 import Error
 
+logger = logging.getLogger(__name__)
+
+
+def row2dict(cursor, row):
+    '''Utility function for sqlite3 row factory to use to convert retrieved row data to dict.
+
+    Args:
+        cursor (<sqlite3.Cursor>): cursor object retrieved from database connection.
+        row (tuple): database row data.
+    Returns:
+        dict: retrived row data with database column labels for keys.
+    '''
+    rowdict = {}
+    for idx, col in enumerate(cursor.description):
+        rowdict[col[0]] = row[idx]
+    return rowdict
+
 
 class Database(object):
-    '''
+    '''Interface to sqlite FoW database.
 
+    Instantiation opens the names database, it must be closed manually when it's time to disconnect.
+
+    TODO: make a base class and subclass it for different database interfaces.
+
+    Args:
+        db_name (str): the full name (including path) of the FoW database to use.
     '''
 
     @property
     def conn(self):
-        ''''''
+        '''<sqlite3.Connection>: connection object to FoW database.'''
         return self._conn
 
-    def select_tank(self, name):
-        '''
+    def select_tank(self, tank_name):
+        '''Select the (assumed) unique data for the named tank team.
 
         Args:
-            name:
-
+            tank_name (str): request characteristics from the database for this specific tank team name.
         Returns:
-            dict:
+            dict: the tank_name characteristics or None.
         '''
-        def row2dict(cursor, row):
-            d = {}
-            for idx, col in enumerate(cursor.description):
-                d[col[0]] = row[idx]
-            return d
-
+        row = None
         try:
             self.conn.row_factory = row2dict
             cur = self.conn.cursor()
-            cur.execute("SELECT * FROM tank_teams WHERE Name=?", (name,))
-            return cur.fetchone()
+            cur.execute("SELECT * FROM tank_teams WHERE Name=?", (tank_name,))
+            row = cur.fetchone()
         except Error as exc:
-            print(exc)
-        return None
+            logging.error('query failed for tank team name {}, {}'.format(tank_name, exc))
+        else:
+            if row is None:
+                logging.error('query failed, tank team name {} not found'.format(tank_name))
+        return row
 
-    def _create_connection(self, db_file):
-        '''create a database connection to the SQLite database specified by the db_file
-
-        Args:
-            db_file: database file name
-
-        Returns:
-            Connection object or None
+    def disconnect(self):
+        '''Close the database connection.
         '''
-        try:
-            conn = sqlite3.connect(db_file)
-            return conn
-        except Error as exc:
-            print(exc)
-        return None
+        self.conn.close()
 
-    def __init__(self, db_file):
-        self._conn = self._create_connection(db_file)
-        if self.conn is None:
-            print('connection not established')
+    def __init__(self, db_name):
+        self._conn = sqlite3.connect(db_name)
---
+++ src/py/test/test_database.py
@@ -0,0 +1,58 @@
+from unittest import TestCase
+from Database import Database
+
+VALID_DB_NAME = '/Applications/PyCharm.app/Contents/bin/fow.sqlite'
+INVALID_DB_NAME = '/Applications/PyCharm.app/Contents/bin/fow.sqlit'
+
+
+class TestDatabase(TestCase):
+
+    def setUp(self):
+        print('\n***** executing {}'.format(self._testMethodName))
+
+    def test_conn(self):
+        '''Apparently connecting to a database can not fail. sqlite3 creates whatever if it does not exist.
+        '''
+        database = Database(VALID_DB_NAME)
+        self.assertTrue(database.conn is not None)
+        database.disconnect()
+        database = Database(INVALID_DB_NAME)
+        self.assertTrue(database.conn is not None)
+        database.disconnect()
+
+    def test_select_tank_valid_db_invalid_name(self):
+        '''Should not find invalid tank name.
+        '''
+        database = Database(VALID_DB_NAME)
+        tank = database.select_tank('Pz')
+        self.assertTrue(tank is None)
+        database.disconnect()
+
+    def test_select_tank_invalid_db_valid_name(self):
+        '''Should not find the tank_teams table since db is invalid.
+        '''
+        database = Database(INVALID_DB_NAME)
+        tank = database.select_tank('Panzer IV H')
+        self.assertTrue(tank is None)
+        database.disconnect()
+
+    def test_select_tank_valid_db_valid_name(self):
+        '''All is well.
+        '''
+        database = Database(VALID_DB_NAME)
+        tank = database.select_tank('Panzer IV H')
+        self.assertTrue(tank is not None)
+        database.disconnect()
+
+    def test_disconnect(self):
+        '''Apparently this can't fail, either. sqlite3 will open up -something- first, which may then be closed.
+        '''
+        database = Database(VALID_DB_NAME)
+        database.disconnect()
+
+    def test_disconnect_closed(self):
+        '''Can't operate on a closed database.
+        '''
+        database = Database(VALID_DB_NAME)
+        database.disconnect()
+        _ = database.select_tank('Panzer IV H')
